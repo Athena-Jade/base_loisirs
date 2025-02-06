73198c4f960d4f216015b4cd7045b69f61efac92
Athena-Jade <athenajade72@gmal.com>
2025-02-06 14:24:15 +0100

    first commit

---
+++ src/composants/Carousel/DataCarousel.jsx
@@ -0,0 +1,29 @@
+import canoe from "../../assets/canoe.jpg";
+import paddle from "../../assets/paddle.jpg";
+import tyrolienne from "../../assets/tyrolienne.jpg";
+
+export const images={
+  canoe,
+  paddle,
+  tyrolienne
+}
+
+
+export const DataCarousel = [
+  {
+    id:1,
+    image: "canoe.jpg",
+    title: "Faites une ballade en canoë,",
+  },
+  {
+    id:2, 
+    image: "tyrolienne.jpg",
+    title: "Ou bien une descente en tyrolienne",
+  },
+  {
+    id:3,
+    image: "paddle.jpg",
+    title: "A moins que vous préfériez le calme",
+  }
+  
+];
